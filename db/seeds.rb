6f97b49b51986e5b95ebd10e0e151554bf80759b
Joathan Francisco <joathanf@gmail.com>
2019-03-14 02:13:58 -0300

    Implementation models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Country.create abbreviation: 'BRA', name: 'BRAZIL'
+ 
+Country.create abbreviation: 'USA', name: 'UNITED STATES OF AMERICA'
+
+User.create name: 'Isaac', email: 'isaac@gmail.com', phone_number: 5556767, country: Country.first, is_admin: true
+
+User.create name: 'Stephen', email: 'stephen@yahoo.com', phone_number: 5558989, country: Country.second, is_admin: false
+
+User.create name: 'Albert', email: 'albert@outlook.com', phone_number: 5554545, country: Country.second, is_admin: false
